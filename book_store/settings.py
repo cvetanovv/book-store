5907fd940199f66bfaa8787c22987532f4bf1b0f
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-03-20 16:42:56 +0200

    Add signup, login and logout functionality

--- book_store/settings.py
+++ book_store/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'pages',
     'accounts',
 ]
@@ -130,3 +131,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
